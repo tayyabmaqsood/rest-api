cec346a48da46f8973b1d45506a568a1c712ae47
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-11-16 00:43:43 +0500

    - add necessary comments in routes

--- routes/feed.js
+++ routes/feed.js
@@ -3,7 +3,12 @@ const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
+//  GET /feed/posts
+// show all post
 router.get('/posts', feedController.getPost);
+
+// POST /feed/post
+// adding new post
 router.post('/post', feedController.createPost);
 
 module.exports = router;
