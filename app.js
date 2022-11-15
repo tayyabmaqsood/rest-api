899854c8453515ba4eb650fac921bcc7fd861d53
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-11-15 18:52:55 +0500

    - headers for same client server added

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const app = express()
 
 app.use(bodyParser.json()); //application/json
 
+
+// every response we sent have the following headers
+app.use((req, res, next)=>{
+
+    // we set header that allow domain to access the server.
+    // we can add server address instead of * , i.e codepen.io
+    res.setHeader('Access-Controll-Allow-Origin', '*');
+    res.setHeader('Access-Controll-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Controll-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 // GET /feed/posts
 // every request starts with /feed, forward towards the feedRoute
 app.use('/feed', feedRouter);
