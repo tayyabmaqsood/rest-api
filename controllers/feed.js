817577d4923bad1eca9f5826d7f982a8136e9536
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-11-16 14:35:20 +0500

    - getPost method updated according to database

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,22 +3,23 @@ const Post = require('../models/post');
 
 // we can access this post by type direct url in the browser
 exports.getPost = (req, res, next)=>{
-    res.status(200).json({
-        posts:[
-            {
-                _id: '1',
-                title: 'First Post', 
-                content: 'This is first post!', 
-                imageUrl:"images/apple.jpg",
-                creator: {
-                    name: "Muhammad Tayyab",
-                },
-                createdAt: new Date()
-            
-            },
 
-        ]
-    });
+    Post.findAll()
+        .then(posts =>{
+            res.status(200).json({
+                message:"Fetched Posts successfully", 
+                posts: posts
+            })
+        })
+        .catch(err =>  {
+            // we are inside the promise chain just `throw` error will not work
+            // we have to use next function to send error to next middleware
+            if (!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+
 };
 
 
