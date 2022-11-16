c2bc2660e066c11777817ce4acb406b33f708ad2
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-11-16 14:04:45 +0500

    - update createPost method to add post in database

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator/check');
+const Post = require('../models/post');
 
 // we can access this post by type direct url in the browser
 exports.getPost = (req, res, next)=>{
@@ -31,19 +32,26 @@ exports.createPost = (req, res, next)=>{
     }
     const title = req.body.title;
     const content = req.body.content;
+    const post = new Post({
+        title:title, 
+        content: content,
+        creator:  'Muhammad Tayyab',
+        imageUrl: 'images/apple.jpg'
+    })
+   
+    post
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message:'POST created successfully',
+                post: result
+            });
+        })
+        .catch(err =>  console.log(err))
+
+
     // creating post in database
     // 201 status code means success and resource is created
-    res.status(201).json({
-        message:'POST created successfully',
-        post: {
-            _id: new Date().toISOString(), 
-            title:title, 
-            content: content,
-            creator: {
-                name: 'Muhammad Tayyab'
-            },
-            createdAt: new Date()
-        },
-
-    });
+    
 };
