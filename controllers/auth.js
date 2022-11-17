d1414040c1af96c34525ba37b4e7505bdb8d0f15
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-11-17 22:17:11 +0500

    - password encryption added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,8 @@
-const User = require('../models/user');
 const {validationResult} = require('express-validator/check')
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+
 
 exports.signup = (req, res, next)=>{
     const errors = validationResult(req);
@@ -12,4 +15,26 @@ exports.signup = (req, res, next)=>{
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
+    bcrypt.hash(password, 12)
+        .then(hashedPassword =>{
+            const user = new User({
+                email: email,
+                password: hashedPassword,
+                name:  name
+            });
+            return user.save()
+        })
+            .then(result =>{
+                res.status(201)
+                    .json({
+                        message: 'User created',
+                        userId: result.id
+                    })
+            })
+        .catch(err =>{
+            if (!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 }
